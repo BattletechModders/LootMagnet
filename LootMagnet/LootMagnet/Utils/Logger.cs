e6d58e5df865b11cdc759343217c94cc0a7433a3
Gerrit Kitts <gkitts@gmail.com>
2020-05-05 15:57:36 -0400

    - Adding HBS logging as a backup

--- LootMagnet/LootMagnet/Utils/Logger.cs
+++ LootMagnet/LootMagnet/Utils/Logger.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HBS.Logging;
+using System;
 using System.IO;
 
 namespace LootMagnet {
@@ -6,6 +7,7 @@ namespace LootMagnet {
 
         private static StreamWriter LogStream;
         private static string LogFile;
+        private readonly ILog HBSLogger;
 
         public Logger(string modDir, string logName) {
             if (LogFile == null) {
@@ -16,15 +18,41 @@ namespace LootMagnet {
             }
 
             LogStream = File.AppendText(LogFile);
+
+            HBSLogger = HBS.Logging.Logger.GetLogger(Mod.HarmonyPackage);
         }
 
         public Logger() { }
 
-        public void Debug(string message) { if (Mod.Config.Debug) { Info(message); } }
+        public void Debug(string message)
+        {
+            if (Mod.Config.Debug)
+            {
+                Log(message);
+            }
+        }
 
-        public void Info(string message) {
-            if (Logger.LogStream == null) { return;  }
+        public void Info(string message)
+        {
+            Log(message);
+        }
 
+        public void Warn(string message)
+        {
+            Log("WARNING:" + message);
+            HBSLogger.LogAtLevel(LogLevel.Warning, "<LOOTMAGNET>" + message);
+        }
+
+        public void Error(string message, Exception e)
+        {
+            Log("ERROR:" + message);
+            Log("ERROR:" + e.Message);
+
+            HBSLogger.LogAtLevel(LogLevel.Error, "<LOOTMAGNET>" + message, e);
+        }
+
+        private void Log(string message)
+        {
             string now = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff", System.Globalization.CultureInfo.InvariantCulture);
             LogStream.WriteLine($"{now} - {message}");
             LogStream.Flush();
