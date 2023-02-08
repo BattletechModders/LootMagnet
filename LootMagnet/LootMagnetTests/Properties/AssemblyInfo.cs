13a38e0dfbc1507df4d40aaa0da571599e122af8
IceRaptor <gkitts@gmail.com>
2023-02-08 12:48:35 -0500

    - Update to new project format

--- LootMagnet/LootMagnetTests/Properties/AssemblyInfo.cs
+++ LootMagnet/LootMagnetTests/Properties/AssemblyInfo.cs
@@ -1,20 +1,7 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("LootMagnetTests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LootMagnetTests")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("4e9d92f2-449c-4d33-aada-f99b06156823")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
